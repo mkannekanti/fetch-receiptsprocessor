c842a4ef311d85b6e53950a7649634ddeefcaee6
mkannekanti <mohan.kannekanti@gmail.com>
2025-04-23 23:11:30 -0700

    Initial Commit
    1. Created Sprint Boot Web Service
    2. Added OpenAPI template
    3. Added Maven OpenAPI plugin to generate Apis and Modules.

---
+++ src/main/java/org/fetch/receiptsprocessor/ReceiptsProcessorApplication.java
@@ -0,0 +1,13 @@
+package org.fetch.receiptsprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptsProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiptsProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/fetch/receiptsprocessor/ReceiptsProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fetch.receiptsprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptsProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
